08fcd592148d5614a2fad95d3f9265b81490adfc
MariaJenson96 <mariajenson1996@gmail.com>
2025-04-22 15:00:23 +0530

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
+import { tabsRoutes } from './routes/tabs.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TabsComponent,
+        children: tabsRoutes,
+      },
+];
