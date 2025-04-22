e0747136a87d8d979b737a033f14577296aae536
MariaJenson96 <mariajenson1996@gmail.com>
2025-04-22 15:08:07 +0530

    as

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
-import { tabsRoutes } from './routes/tabs.routes';
 
 export const routes: Routes = [
     {
         path: '',
-        component: TabsComponent,
-        children: tabsRoutes,
+        component: TabsComponent
       },
 ];
